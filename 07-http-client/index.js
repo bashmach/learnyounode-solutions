fb3cef0df5619b29c0b36befcaa10fd042b6f5de
Pavel Machekhin <pavel.machekhin@gmail.com>
2016-03-12 17:47:11 +0200

    07-HTTP-CLIENT

---
+++ 07-http-client/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const http = require('http');
+
+http.get(process.argv[2], function(response) {
+  response.setEncoding('utf-8');
+
+  response.on('data', function(data) {
+    console.log(data);
+  });
+  response.on('error', function(e) {
+    throw e;
+  });
+});
